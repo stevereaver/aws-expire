bce1aa0613d714a199e1aa4ae4a209d8d4203090
Stephen Bancroft <stevereaver@gmail.com>
2019-02-14 15:38:23 +1100

    Create aws_cron.sh

---
+++ aws_cron.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+#
+# --------------------------------------------------------------------------
+# Name:         zfs cron
+# By:           Stephen Bancroft
+# Email:        stevereaver@gmail.com
+# Date:         13/02/19
+#
+# Desc:         This script is called from zabbix's cron and is used to 
+#               collect the state of the AWS instance
+#
+# Usage:        aws_cron.sh
+# --------------------------------------------------------------------------
+#
+
+RESINST=~scripts/aws/describe_reserved_instances.py
+SENDER=/usr/bin/zabbix_sender
+ZABBIX=zabbix.local
+PORT=10050
+
+$RESINST --profile itg | $SENDER -i - -z $ZABBIX -p $PORT > /dev/null 2>&1
+$RESINST --profile sws | $SENDER -i - -z $ZABBIX -p $PORT > /dev/null 2>&1
